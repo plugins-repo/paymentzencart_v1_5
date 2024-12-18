c1c0a95c925ae4095e6427149bdf3d309a9fe737
plugins-repo <mahesh.biradar@paymentz.com>
2024-12-18 14:58:51 +0530

    Update paymentzencart_v1_5.php

--- includes/languages/english/modules/payment/paymentzencart_v1_5.php
+++ includes/languages/english/modules/payment/paymentzencart_v1_5.php
@@ -1,1 +1,6 @@
+<?php
 
+define('MODULE_PAYMENT_PAYMENTZENCART_V1_5_TEXT_TITLE', 'Paymentzencart_v1_5 Payment Gateway');
+define('MODULE_PAYMENT_PAYEMENTZENCART_V1_5_TEXT_DESCRIPTION', 'This module allows payments via the Paymentzencart_v1_5 payment gateway.');
+
+?>
